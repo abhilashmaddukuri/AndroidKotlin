cc0a018e8a7caef75b3eae8460c448f868732f30
Abhilash Maddukuri <abhilashmaddukuri@gmail.com>
2020-07-22 16:25:32 -0700

    Initial commit

---
+++ app/src/main/java/com/fresnostate/myapplication/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.fresnostate.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show()
+
+        /*var test: Int = 4
+        var test2: String? = "Hello World"
+        val test3: String = "DO NOT CHANGE THIS"
+
+        test2 = null
+        "lkfajdlj" + "ajdkl ${1+1} jfkljd"
+            var demo: String = test2?:"Hello"
+
+        val numberOfFish = 5
+        val numberOfPlants = 12
+        var tt: String = "I have ${numberOfFish + numberOfPlants} fish and plants" + " klji"
+
+
+        when (numberOfFish) {
+            0 -> println("Hello")
+            in 1..60 -> println("1..60")
+            else -> println("oops!!")
+        }
+
+        var fishFoodTreats: Int? = null
+        fishFoodTreats = fishFoodTreats?.dec()
+
+        var listExample = mutableListOf<Int>(1, 4, 24)
+        var arrayExample = arrayOf("one", "two", "three", "four")
+
+        val school = arrayOf("shark", "salmon", "minnow")
+        for ((index, element) in school.withIndex()) {
+
+        }
+
+
+
+        listExample.remove(24)
+        listExample.add(55)
+        Toast.makeText(this, "testing demo ${listExample[2]}", Toast.LENGTH_SHORT).show()*/
+
+
+
+
+    }
+}
